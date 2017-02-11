d67b65d674f5a68e96e67e693be95155cf5e6e9b
Christopher Pezza <pezza.chris@gmail.com>
2017-02-11 09:24:53 -0500

    Fix installer

--- lib/generators/devise_materialize/install_generator.rb
+++ lib/generators/devise_materialize/install_generator.rb
@@ -5,7 +5,7 @@ module DeviseMaterialize
 
     desc "Creates a Devise Materialize Styled Views"
     argument :namespace, type: :string, defualt: "Users"
-    class_option :simple_form, type: :boolean, default: false,
+    class_option :form_engine, type: :string, default: "default",
                                description: "Define if simple form is used " \
                                "to load views built with simple form"
     class_option :template_engine, type: :string, default: "erb", alias: "-e",
@@ -13,35 +13,9 @@ module DeviseMaterialize
                                    "from erb, haml, or slim"
 
     def generate_views
-      if options.simple_form
-        simple_form_views(options.template_engine.downcase)
-      else
-        default_views(options.template_engine.downcase)
-      end
-    end
-
-    private
-
-    def simple_form_views(data)
-      case data
-      when "erb"
-        directory "erb/simple_form", "app/views/#{namespace.downcase}"
-      when "haml"
-        directory "haml/simple_form", "app/views/#{namespace.downcase}"
-      when "slim"
-        directory "slim/simple_form", "app/views/#{namespace.downcase}"
-      end
-    end
-
-    def default_views(data)
-      case data
-      when "erb"
-        directory "erb/defaults", "app/views/#{namespace.downcase}"
-      when "haml"
-        directory "haml/defaults", "app/views/#{namespace.downcase}"
-      when "slim"
-        directory "slim/defaults", "app/views/#{namespace.downcase}"
-      end
+      puts "Generating Views"
+      directory "#{template_engine.downcase}/#{form_engine}",
+                "app/views/#{namespace.downcase}"
     end
   end
 end
