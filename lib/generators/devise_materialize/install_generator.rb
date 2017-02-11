8b3ed7c48197c8cd3ceb6721a520eeefcd7a89d5
Christopher Pezza <pezza.chris@gmail.com>
2017-02-11 09:31:56 -0500

    change class option name

--- lib/generators/devise_materialize/install_generator.rb
+++ lib/generators/devise_materialize/install_generator.rb
@@ -1,20 +1,22 @@
 # frozen_string_literal: true
+require "rails/generators"
+
 module DeviseMaterialize
   class InstallGenerator < Rails::Generators::Base
     source_root File.expand_path("../../templates", __FILE__)
 
     desc "Creates a Devise Materialize Styled Views"
-    argument :namespace, type: :string, defualt: "Users"
+    argument :namespace, type: :string, default: "Users"
     class_option :form_engine, type: :string, default: "default",
                                description: "Define if simple form is used " \
                                "to load views built with simple form"
-    class_option :template_engine, type: :string, default: "erb", alias: "-e",
-                                   description: "Choose a template engine " \
-                                   "from erb, haml, or slim"
+    class_option :view_engine, type: :string, default: "erb", alias: "-e",
+                               description: "Choose a template engine " \
+                               "from erb, haml, or slim"
 
     def generate_views
       puts "Generating Views"
-      directory "#{template_engine.downcase}/#{form_engine}",
+      directory "#{view_engine.downcase}/#{form_engine}",
                 "app/views/#{namespace.downcase}"
     end
   end
