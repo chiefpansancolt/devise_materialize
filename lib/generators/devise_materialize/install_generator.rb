38558c027597f2f54da442d38b1c36951825a98e
Christopher Pezza <pezza.chris@gmail.com>
2017-02-11 11:06:17 -0500

    Update default namespace to Devise instead of User

--- lib/generators/devise_materialize/install_generator.rb
+++ lib/generators/devise_materialize/install_generator.rb
@@ -6,16 +6,16 @@ module DeviseMaterialize
     source_root File.expand_path("../../templates", __FILE__)
 
     desc "Creates a Devise Materialize Styled Views"
-    argument :namespace, type: :string, default: "Users"
+    argument :namespace, type: :string, default: "Devise"
     class_option :form_engine, type: :string, default: "default",
-                               description: "Define if simple form is used " \
-                               "to load views built with simple form"
-    class_option :view_engine, type: :string, default: "erb", alias: "-e",
-                               description: "Choose a template engine " \
-                               "from erb, haml, or slim"
+                               description: "Choose a form engine " \
+                               "(default or simple_form)"
+    class_option :view_engine, type: :string, default: "erb",
+                               description: "Choose a views engine " \
+                               "(erb, haml, or slim)"
 
     def generate_views
-      puts "Generating Views ....."
+      puts "Generating Views....."
       directory "#{options.view_engine.downcase}/#{options.form_engine}",
                 "app/views/#{namespace.downcase}"
     end
