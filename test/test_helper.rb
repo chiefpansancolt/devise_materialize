b8b3cd6089c805f0052ad8e97253c94427eeea7e
Christopher Pezza <pezza.chris@gmail.com>
2017-02-11 16:24:30 -0500

    Fix minitest-ci

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,11 +3,18 @@ ENV["RAILS_ENV"] = "test"
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 
 require "simplecov"
+require "devise_materialize"
 require "rails_app/config/environment"
 require "rails/test_help"
+require "minitest"
 require "minitest/spec"
 require "minitest/pride"
-require "devise_materialize"
+require "minitest/autorun"
+require "minitest/ci"
+
+if ENV["CIRCLE_TEST_REPORTS"].present?
+  Minitest::Ci.report_dir = "#{ENV['CIRCLE_TEST_REPORTS']}/reports"
+end
 
 SimpleCov.formatters = [
   SimpleCov::Formatter::HTMLFormatter
