61fc7d5452298cd3061b42ed325f7679a5492355
Christopher Pezza <pezza.chris@gmail.com>
2018-02-11 20:08:08 -0500

    Update Simplecov config

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,20 @@
 # frozen_string_literal: true
 
+require "simplecov"
+
+SimpleCov.formatter = SimpleCov::Formatter::HTMLFormatter
+
+SimpleCov.start do
+  add_filter "/test/"
+  add_group "Library", "lib/"
+end
+
+SimpleCov.minimum_coverage_by_file 90
+SimpleCov.minimum_coverage 90
+
 ENV["RAILS_ENV"] = "test"
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 
-require "simplecov"
 require "devise_materialize"
 require "rails_app/config/environment"
 require "rails/test_help"
@@ -12,20 +23,6 @@ require "minitest/spec"
 require "minitest/pride"
 require "minitest/autorun"
 require "minitest/ci"
-
-if ENV["CIRCLE_TEST_REPORTS"].present?
-  Minitest::Ci.report_dir = "#{ENV['CIRCLE_TEST_REPORTS']}/reports"
-end
-
-SimpleCov.formatters = [
-  SimpleCov::Formatter::HTMLFormatter
-]
-
-SimpleCov.start do
-  add_filter "/test/"
-
-  add_group "Library", "lib"
-end
 
 # For generators
 require "rails/generators/test_case"
